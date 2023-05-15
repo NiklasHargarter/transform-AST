e4def9d28e10a3cd115503c7bd5a6af7721c6e7e
NiklasHargarter <91331727+NiklasHargarter@users.noreply.github.com>
2023-05-15 09:34:51 +0200

    Use local language-java

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,6 +7,7 @@ where
 
 import Data.Generics.Uniplate.Data (universeBi)
 import Language.Java.Parser
+import Language.Java.SourceSpan (dummySourceSpan)
 import Language.Java.Syntax
 import System.IO (IOMode (ReadMode), char8, hGetContents, hSetEncoding, openFile)
 
@@ -17,10 +18,7 @@ someFunc = do
   let ecUnit = parser compilationUnit path2 file
   case ecUnit of
     Right cUnit -> do
-      -- print (printClass cUnit)
-      -- print (printMethod cUnit)
-      -- print (classifyInvocations cUnit)
-      print cUnit
+      print (testClass cUnit)
     Left _ -> putStrLn "parse failed"
   putStrLn "someFunc"
 
@@ -31,25 +29,11 @@ readFileCustom path = do
   file <- hGetContents inputHandle
   return (path, file)
 
-data ClassifiedName
-  = ExpressionName
-  | TypeName
-  | PackageName
-  | Ambigous
-  deriving (Show)
-
-classifyInvocations :: CompilationUnit -> [(MethodInvocation, ClassifiedName)]
-classifyInvocations cUnit = do
-  classDecl <- universeBi cUnit
-  let fields = classFields classDecl
-  method@(MethodCall (Name [id, _]) _) <- universeBi cUnit
-  if id `elem` fields then return (method, ExpressionName) else return (method, Ambigous)
-
-identsfromDecls :: [Decl] -> [Ident]
+identsfromDecls :: [Decl p] -> [Ident]
 identsfromDecls decls = do
   universeBi
     ( map
-        (\(VarDecl id _) -> id)
+        (\(VarDecl _ id _) -> id)
         ( concatMap
             ( \case
                 (MemberDecl (FieldDecl _ _ _ vars)) -> vars
@@ -59,36 +43,36 @@ identsfromDecls decls = do
         )
     )
 
-classFields :: ClassDecl -> [Ident]
+classFields :: ClassDecl Parsed -> [Ident]
 classFields (ClassDecl _ _ _ _ _ _ (ClassBody decls)) = identsfromDecls decls
 classFields (RecordDecl _ _ _ _ _ _ (ClassBody decls)) = identsfromDecls decls
 classFields (EnumDecl _ _ _ _ (EnumBody _ decls)) = identsfromDecls decls
 
-identsFromParams :: [FormalParam] -> [Ident]
+identsFromParams :: [FormalParam p] -> [Ident]
 identsFromParams params = do
-  universeBi (map (\(FormalParam _ _ _ vardeclId) -> vardeclId) params)
+  universeBi (map (\(FormalParam _ _ _ _ vardeclId) -> vardeclId) params)
 
-methodParams :: MemberDecl -> [Ident]
+methodParams :: MemberDecl Parsed -> [Ident]
 methodParams (MethodDecl _ _ _ _ _ params _ _ _) = identsFromParams params
 methodParams (ConstructorDecl _ _ _ _ params _ _) = identsFromParams params
 methodParams _ = []
 
-printClass :: CompilationUnit -> [(Ident, [Ident])]
-printClass cUnit = do
+testClass :: CompilationUnit Parsed -> [(Ident, [Ident])]
+testClass cUnit = do
   classDecl <- universeBi cUnit
   return (className classDecl, classFields classDecl)
 
-className :: ClassDecl -> Ident
+className :: ClassDecl Parsed -> Ident
 className (ClassDecl _ _ id _ _ _ _) = id
 className (RecordDecl _ _ id _ _ _ _) = id
 className (EnumDecl _ _ id _ _) = id
 
-printMethod :: CompilationUnit -> [(Ident, [Ident])]
-printMethod cUnit = do
+testMethod :: CompilationUnit Parsed -> [(Ident, [Ident])]
+testMethod cUnit = do
   memberDecl <- universeBi cUnit
   return (memberName memberDecl, methodParams memberDecl)
 
-memberName :: MemberDecl -> Ident
+memberName :: MemberDecl Parsed -> Ident
 memberName (MethodDecl _ _ _ _ id _ _ _ _) = id
 memberName (ConstructorDecl _ _ _ id _ _ _) = id
-memberName _ = Ident "not a method"
+memberName _ = Ident dummySourceSpan "not a method"
